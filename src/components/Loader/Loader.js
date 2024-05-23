631cf6795c9134afc1a7d5532c981446d413006c
Sowndharya_Maheswaran <102723905+SOWNDHARYA-MAHESWARAN@users.noreply.github.com>
2024-05-23 22:40:34 +0530

    Update Loader.js

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,21 +1,27 @@
 import React from "react";
 import LoaderImg from "../../images/Loader.svg";
 import "./Loader.css";
-import { red } from "@mui/material/colors";
 
 const Loader = () => {
   return (
     <div className="loader flex flex-c">
-      <p>
+      <p className="loading-message">
+        <span className="star">*</span>
         Initial loading may take 5 to 60 seconds due to API throttling.
+        <br />
         Meanwhile, watch the{" "}
-        <a href="https://www.loom.com/share/c6b5876f147f47d69b7b6798db39dc8a?sid=e473dccf-db30-444e-8696-bceb3c57c120">
+        <a
+          href="https://www.loom.com/share/c6b5876f147f47d69b7b6798db39dc8a?sid=e473dccf-db30-444e-8696-bceb3c57c120"
+          className="demo-link"
+        >
           demo
         </a>
-        . This can be optimized in a production environment.
+        .
+        <br />
+        This can be optimized in a production environment.
       </p>
       <img src={LoaderImg} alt="loader" />
-      <p>Please wait for few seconds</p>
+      <p>Please wait for a few seconds</p>
     </div>
   );
 };
