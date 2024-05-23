8905301ce5f9d6130a78f66c8e1240e076219c06
Sowndharya_Maheswaran <102723905+SOWNDHARYA-MAHESWARAN@users.noreply.github.com>
2024-05-23 22:28:43 +0530

    Update Loader.js

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -6,7 +6,7 @@ import { red } from "@mui/material/colors";
 const Loader = () => {
   return (
     <div className="loader flex flex-c">
-      <p style={{ fontSize: "15px", color: red }}>
+      <p>
         Initial loading may take 5 to 60 seconds due to API throttling.
         Meanwhile, watch the{" "}
         <a href="https://www.loom.com/share/c6b5876f147f47d69b7b6798db39dc8a?sid=e473dccf-db30-444e-8696-bceb3c57c120">
